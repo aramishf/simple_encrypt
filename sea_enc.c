df4473d980f25e8c3a6af699f5324c5578e008a6
Aramish Farooq <aramishf@Aramishs-MacBook-Air.local>
2026-02-03 17:08:10 -0800

    Initial commit with basic code

---
+++ sea_enc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// one hex digit = 4 bits (nibble)
+// 4 hex digits = 16 bits = 2 bytes
+
+int main() {
+    int N = 0x0120;                                 // hexa decimal value
+    int input;                                      // 16-bit key, inputed as hexadecimal value
+
+    printf("Type a hexadecimal key (0xABCD): \n");
+    scanf("%d", &input);
+
+
+
+
+
+
+
+}
