d825a215ff30129e2a83f162dba227ab4c67afae
Aramish Farooq <aramishf@Aramishs-MacBook-Air.local>
2026-02-05 11:24:22 -0800

    Finishing up

--- sea_enc.c
+++ sea_enc.c
@@ -12,12 +12,41 @@ int main(int argc, char *argv[]) {
   // if no key provided, return -1
   if (argc < 2) {
     printf("Usage: ./sea_enc KEY_IN_HEX\n");
-    return -1;
+    return 1;
   }
 
   // convert key form hex to int
   // argv[1] - first arg passed to program
-  int N = strtol(argv[1], NULL, 16);
+  // unsigned short for easy bit manipulation
+  unsigned short N = (unsigned short)strtol(argv[1], NULL, 16);
 
   // Read input bytes from stdin one by one
+  unsigned char input_byte;
+  int count = 0;
+
+  // while loop to read bytes from stdin
+  // read() returns number of bytes read
+  // file descriptor 0 is stdin
+  while (read(0, &input_byte, 1) > 0) {
+
+    // XOR N with input byte and get lower 8 bits
+    unsigned char encrypted = (N ^ input_byte) & 0xFF;
+
+    // Output as two upper case hex char (%02x handles nibble encoding)
+    printf("%02X", encrypted);
+    count++;
+
+    // rol 1 bit
+    // for 16 bit: bit 15 moves to bit 0
+    // bit 14 moves to bit 13, etc
+    N = (N << 1) | (N >> 15) & 0xFFFF;
+
+    // multiply by 257 and keep lower 16 bits
+    N = (N * 257) & 0xFFFF;
+  }
+
+  // print newline after all bytes are encrypted
+  printf("\n");
+
+  return 0;
 }
