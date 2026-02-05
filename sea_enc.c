10057c585324353c867cecd13bd43b432e093c84
Aramish Farooq <aramishf@Aramishs-MacBook-Air.local>
2026-02-04 17:23:40 -0800

    Argument counter and argument vectorfor process startup

--- sea_enc.c
+++ sea_enc.c
@@ -4,16 +4,22 @@
 // 4 hex digits = 16 bits = 2 bytes
 
 int main() {
-    int N = 0x0120;                                 // hexa decimal value
-    int input;                                      // 16-bit key, inputed as hexadecimal value
+  int N = 0x0001; // hexa-decimal value                                    //
+                  // 16-bit key, inputed as hexadecimal value
 
-    printf("Type a hexadecimal key (0xABCD): \n");
-    scanf("%d", &input);
+  // open file for reading "r"
+  FILE *file = fopen("input.txt", "r");
 
+  // check if file is not empty
+  if (file != NULL) {
+    char *input[256];
 
+    // read the input from the file
+    fscanf(file, "%x", &input[256]);
 
+    // close the file
+    fclose(file);
+  }
 
-
-
-
+  // convert hexadecimal input to binary
 }
